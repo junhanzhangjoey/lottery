166ce4cc5c8f4c8ff44a6aa20c01ae594ed65228
junhanzhangjoey <jh1084430069@gmail.com>
2025-02-19 00:57:12 +0000

    second version

--- lottery.c
+++ lottery.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-
+#include <stdbool.h>
 int generate(int n,int r, int p, int N){
   int randomN=0;
   int powerB=0;
@@ -26,32 +26,50 @@ int generate(int n,int r, int p, int N){
 }
 
 int main(int argc, char *argv[]){
+  
   int opt;
   int NumbersToGenerate;
   int MaxNumber;
   int MaxPowerBallNumber=-1;
   int NumberSetsToGenerate;
+  bool n,r,p,N;
+  n=r=N=false;
+  p=true;
   while((opt=getopt(argc,argv,"n:r:p:N:"))!=-1){
     switch(opt)
     {
       case 'n':
-	NumbersToGenerate=atoi(optarg);
+     	NumbersToGenerate=atoi(optarg);
+     	if(NumbersToGenerate>0){
+     	   n=true;
+     	}
 	break;
       case 'r':
-	MaxNumber=atoi(optarg);
+      	MaxNumber=atoi(optarg); 
+      	if(MaxNumber>0){
+     	   r=true;
+     	}
 	break;
       case 'p':
-      	if(optarg!=NULL){
-      	   MaxPowerBallNumber=atoi(optarg);
-      	}
+      	MaxPowerBallNumber=atoi(optarg);
+      	if(MaxPowerBallNumber<=0){
+     	   p=false;
+     	}
 	break;
       case 'N':
-	NumberSetsToGenerate=atoi(optarg);
+      	NumberSetsToGenerate=atoi(optarg);
+      	if(NumberSetsToGenerate>0){
+     	   N=true;
+     	}
 	break;
       default:
       	break;
     }
   }
-  generate(NumbersToGenerate,MaxNumber,MaxPowerBallNumber,NumberSetsToGenerate);
-  return 0;
+if(!(n&&r&&p&&N)){
+   fprintf(stderr, "Input format is wrong\n");
+   return 1;
+} 
+generate(NumbersToGenerate,MaxNumber,MaxPowerBallNumber,NumberSetsToGenerate);
+return 0;
 }
