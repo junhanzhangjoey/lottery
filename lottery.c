1289fa4d692abec3aa08ea8776d30188b0fa83b4
junhanzhangjoey <jh1084430069@gmail.com>
2025-02-19 21:54:52 +0000

    no repeated numbers

--- lottery.c
+++ lottery.c
@@ -3,16 +3,28 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdbool.h>
+
 int generate(int n,int r, int p, int N){
   int randomN=0;
   int powerB=0;
-  srand(time(NULL));
+  bool repeat[r];
   for(int i=0;i<N;i++){
+  
+     for(int k=0;k<r;k++){
+        repeat[k]=false;
+     }
+     
      for(int j=0;j<n;j++){
-     	randomN=rand() % (r+1);
+     	randomN=rand() % r+1;
+     	
+     	while(repeat[randomN-1]){
+     	   randomN=rand() % r+1;
+     	}
+     	repeat[randomN-1]=true;
+     	
      	if(j==n-1){
 	   if(p!=-1){
-	      powerB=rand() % (p+1);
+	      powerB=rand() % (p)+1;
      	      printf("%d, Power Ball: %d\n",randomN,powerB);
 	   }else{
 	      printf("%d\n",randomN);
@@ -26,47 +38,48 @@ int generate(int n,int r, int p, int N){
 }
 
 int main(int argc, char *argv[]){
+  srand(time(NULL));
   
   int opt;
   int NumbersToGenerate;
   int MaxNumber;
   int MaxPowerBallNumber=-1;
   int NumberSetsToGenerate;
-  bool n,r,p,N;
-  n=r=N=false;
-  p=true;
+  bool nb,rb,pb,Nb;
+  nb=rb=Nb=false;
+  pb=true;
   while((opt=getopt(argc,argv,"n:r:p:N:"))!=-1){
     switch(opt)
     {
       case 'n':
      	NumbersToGenerate=atoi(optarg);
      	if(NumbersToGenerate>0){
-     	   n=true;
+     	   nb=true;
      	}
 	break;
       case 'r':
       	MaxNumber=atoi(optarg); 
       	if(MaxNumber>0){
-     	   r=true;
+     	   rb=true;
      	}
 	break;
       case 'p':
       	MaxPowerBallNumber=atoi(optarg);
       	if(MaxPowerBallNumber<=0){
-     	   p=false;
+     	   pb=false;
      	}
 	break;
       case 'N':
       	NumberSetsToGenerate=atoi(optarg);
       	if(NumberSetsToGenerate>0){
-     	   N=true;
+     	   Nb=true;
      	}
 	break;
       default:
       	break;
     }
   }
-if(!(n&&r&&p&&N)){
+if(!(nb&&rb&&pb&&Nb)||(NumbersToGenerate>MaxNumber)){
    fprintf(stderr, "Input format is wrong\n");
    return 1;
 } 
